a9614fc57a58399e4ef8f139f771ae3a3d81d2e1
Lazarone Santos Santana <lazarone@localhost.localdomain>
2017-07-11 13:42:36 -0300

    variaveis

--- iniciandoComJavascript/main.js
+++ iniciandoComJavascript/main.js
@@ -1,1 +1,27 @@
 console.log('hello from external script');
+
+var number  = 0;
+var number1 = 1;
+var bool    = true;
+var double  = 0.0;
+
+// typeof retorna o type do atributo
+console.log(typeof number);
+
+number = "0";
+console.log(typeof number);
+
+number = "0" + number1;
+console.log(number);
+
+function myFunction() {
+  var number = 0.1 + 1;
+  console.log(number);
+}
+myFunction();
+
+var myFunction2 = function() {
+  var number = 0.2 + 1;
+  console.log(number);
+}
+myFunction2();
